a83bfaba9edcb142ef85e5913ead2c92ca8fdf12
bawfng04 <nguyendinhbang53az@gmail.com>
2025-02-02 23:43:35 +0700

    add fallback + env file for frontend

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello from backend!" });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { BrowserRouter, Routes } from "react-router-dom";
 import routes from "./routes";
+import { exampleAPI } from "./apis";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>{routes}</Routes>
+        <p>API URL: {exampleAPI}</p>
       </BrowserRouter>
     </div>
   );
